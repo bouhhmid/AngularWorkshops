7a9bc4c9e5d50085c6090c961d4914120fd53111
bouhhmid <147922611+bouhhmid@users.noreply.github.com>
2024-11-26 23:28:33 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResidencesComponent } from './residences/residences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,         // Déclaration du composant principal
+    ResidencesComponent   // Déclaration du composant ResidencesComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,        // Nécessaire pour exécuter l'application Angular dans un navigateur
+    AppRoutingModule,     // Module de gestion des routes
+    FormsModule           // Nécessaire pour utiliser [(ngModel)] dans les formulaires
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],          // Liste des services disponibles dans l'application
+  bootstrap: [AppComponent] // Composant principal qui démarre l'application
 })
 export class AppModule { }
+
+
